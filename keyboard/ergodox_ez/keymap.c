c50713b244d3b65758a8f5a0ea80964fc9a434ca
Dongfeng Yu <yudongfeng@alumni.sjtu.edu.cn>
2020-08-21 16:07:34 +0800

    Updated.

--- keyboard/ergodox_ez/keymap.c
+++ keyboard/ergodox_ez/keymap.c
@@ -623,7 +623,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_UTILITY] = KEYMAP_PERMUTE(
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_NO, KC_NO, LGUI(LSFT(KC_4)), KC_NO, KC_NO,
+        KC_NO, KC_VOLD, KC_VOLU, LGUI(LSFT(KC_4)), KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO, KC_NO, KC_MPLY,
         KC_NO, KC_NO,
@@ -631,7 +631,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_VOLD, KC_VOLU, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO,
